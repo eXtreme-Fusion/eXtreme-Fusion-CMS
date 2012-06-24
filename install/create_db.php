35e466b60881d0e7a5a5d3421e629b2b5724c6ac
Lisu <kontakt@kamillis.pl>
2012-06-24 14:55:22 +0200

    Nowy wzór komentarza licencyjnego

--- install/create_db.php
+++ install/create_db.php
@@ -1,13 +1,14 @@
 <?php
-/*---------------------------------------------------------------+
-| eXtreme-Fusion - Content Management System - version 5         |
-+----------------------------------------------------------------+
-| Copyright (c) 2005-2012 eXtreme-Fusion Crew                	 |
-| http://extreme-fusion.org/                               		 |
-+----------------------------------------------------------------+
-| This product is licensed under the BSD License.				 |
-| http://extreme-fusion.org/ef5/license/						 |
-+---------------------------------------------------------------*/
+/***********************************************************
+| eXtreme-Fusion 5.0 Beta 5
+| Content Management System       
+|
+| Copyright (c) 2005-2012 eXtreme-Fusion Crew                	 
+| http://extreme-fusion.org/                               		 
+|
+| This product is licensed under the BSD License.				 
+| http://extreme-fusion.org/ef5/license/						 
+***********************************************************/
 $result = dbquery("DROP TABLE IF EXISTS ".$db_prefix."pages");
 $result = dbquery("CREATE TABLE ".$db_prefix."pages (
 	`id` SMALLINT UNSIGNED AUTO_INCREMENT,
