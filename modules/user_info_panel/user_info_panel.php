e6b5618ade70721cafbd7e8b61bf4a18c4e6d296
Lisu <kontakt@kamillis.pl>
2012-07-19 14:52:00 +0200

    Wyodrębnienie pliku CSS

--- modules/user_info_panel/user_info_panel.php
+++ modules/user_info_panel/user_info_panel.php
@@ -9,6 +9,7 @@
 | http://extreme-fusion.org/ef5/license/						 |
 +---------------------------------------------------------------*/
 $_locale->moduleLoad('lang', 'user_info_panel');
+$_head->set('<link href="'.ADDR_MODULES.'user_info_panel/stylesheet/user_info_panel.css" media="screen" rel="stylesheet">');
 
 if ($_user->isLoggedIn())
 {
