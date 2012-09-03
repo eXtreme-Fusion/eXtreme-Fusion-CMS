80ea0309d6b55bc774df4a96431046c0d8a404b6
Rafik89 <rafik89@extreme-fusion.pl>
2012-09-03 15:50:33 +0200

    Szkielet modułu forum
    Signed-off-by: Rafik89 <rafik89@extreme-fusion.pl>

---
+++ modules/forum/pages/forum.php
@@ -0,0 +1,15 @@
+<?php defined('EF5_SYSTEM') || exit;
+/*---------------------------------------------------------------+
+| eXtreme-Fusion - Content Management System - version 5         |
++----------------------------------------------------------------+
+| Copyright (c) 2005-2012 eXtreme-Fusion Crew                	 |
+| http://extreme-fusion.org/                               		 |
++----------------------------------------------------------------+
+| This product is licensed under the BSD License.				 |
+| http://extreme-fusion.org/ef5/license/						 |
++---------------------------------------------------------------*/
+$_locale->moduleLoad('lang', 'forum');
+
+! class_exists('Tag') || $_tag = New Tag($_system, $_pdo);
+
+$_tpl->setPageCompileDir(DIR_MODULES.'forum'.DS.'templates'.DS);
---
+++ modules/forum/templates/forum.tpl
@@ -0,0 +1,5 @@
+{php} opentable(__('Strona główna forum')) {/php}
+	<div class="tbl2">
+		<div class="info">{i18n('Tu będzie znajdować się forum.')}</div>
+	</div>
+{php} closetable() {/php}
