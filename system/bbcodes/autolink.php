5672f30a83742e40d64bbc7fe6e02e74698011ac
Inscure <drimer.eco@gmail.com>
2013-02-20 21:21:51 +0100

    Update system/bbcodes/autolink.php

--- system/bbcodes/autolink.php
+++ system/bbcodes/autolink.php
@@ -12,8 +12,8 @@
 | Copyright (C) 2002 - 2011 Nick Jones
 | http://www.php-fusion.co.uk/
 +--------------------------------------------------------+
-| Filename: cronjob.php
-| Author: Nick Jones (Digitanium)
+| Author: Wooya
+| Edited: slawekneo
 +--------------------------------------------------------+
 | This program is released as free software under the
 | Affero GPL license. You can redistribute it and/or
