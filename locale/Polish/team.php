224ec52ac930a3cb92db0b71a16bfaf13acf228f
Michał <wolfram1987@o2.pl>
2013-03-10 12:48:11 +0100

    Update team.php

--- locale/Polish/team.php
+++ locale/Polish/team.php
@@ -1,14 +1,14 @@
 <?php defined('EF5_SYSTEM') || exit;
 
 return array(
-	'Site team' => 'Redakcja strony',
-	'Main administrator' => 'Główny Administrator',
+	'Site Team' => 'Redakcja strony',
+	'Main Administrator' => 'Główny Administrator',
 	'Administrators' => 'Administratorzy',
 	'User' => 'Użytkownik',
-	'Joined:' => 'Dołączył:',
-	'Last visit:' => 'Ostatnia wizyta:',
+	'Joined' => 'Dołączył',
+	'Last Visit' => 'Ostatnia wizyta',
 	'Online' => 'Na stronie',
 	'Offline' => 'Wylogowany',
-	'Groups:' => 'Grupy użytkownika:',
+	'Groups' => 'Grupy użytkownika',
 	'There are no users who belongs to this group.' => 'Brak użytkowników należących do tej grupy.',
 );
