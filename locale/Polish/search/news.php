7e8dadc2b31223c7f57b5872f50c9d43e449f51b
Michał <wolfram1987@o2.pl>
2013-03-10 16:06:52 +0100

    Update news.php

--- locale/Polish/search/news.php
+++ locale/Polish/search/news.php
@@ -2,10 +2,4 @@
 
 return array(
 	'n400' => 'Newsy',
-	'n401' => 'news',
-	'n402' => 'newsów',
-	'n403' => 'znalezionych w',
-	'n404' => 'temacie newsa',
-	'n405' => 'treści newsa',
-	'n406' => 'rozszerzonej treści newsa.'
 );
