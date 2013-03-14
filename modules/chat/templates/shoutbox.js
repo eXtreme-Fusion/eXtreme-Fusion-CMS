2cd0c1a68e3bdd2259d4bc33e8c53df1c70f88e4
Michał <wolfram1987@o2.pl>
2013-03-14 16:32:32 +0100

    Update shoutbox.js

--- modules/chat/templates/shoutbox.js
+++ modules/chat/templates/shoutbox.js
@@ -16,16 +16,16 @@
 		},function() {
 			clearInterval(interval);
 		});
-		
+
 
 		refresh2();
-	  
+
 		$('form#ShoutBoxForm').submit(function() {
-		 
+
 			var action = $('input.InfoBoxButton').attr('name');
-			
+
 			$('input.InfoBoxButton').attr('name', 'send');
-			
+
 			$.post(addr_site+'modules/chat/ajax/send.php', {
 				content: $('textarea[name*="content"]', this).val(),
 				post_id: $('input[name*="post_edit_id"]', this).val(),
@@ -35,10 +35,10 @@
 				$('textarea[name*="content"]').val('');
 				refresh2();
 			  }
-			); 
+			);
 			return false;
 		});
-		
+
 		/* Usuwanie postów */
 		$('#post_shoutbox_delete').live('click', function() {
 			var href = $(this).attr('href');
@@ -47,18 +47,18 @@
 				  refresh2();
 				}, error:function(){
 					$('#ShoutBoxPosts').html('Wystąpił błąd! Odśwież stronę.');
-				}          
+				}
 			  });
 				$('#delete_shoutbox_confirm').remove();
 				$('#ShoutBoxForm').show();
 			return false;
 		});
-		
+
 		$('#post_shoutbox_cancel').live('click', function() {
 			$('#delete_shoutbox_confirm').remove();
 			$('#ShoutBoxForm').show();
 		});
-		
+
 		$('.shoutbox_delete_post').live('click', function() {
 			$('#delete_shoutbox_confirm').remove();
 			var href = $(this).attr('href');
@@ -66,9 +66,9 @@
 			$('#ShoutBoxForm').before('<div id="delete_shoutbox_confirm" class="InfoBoxInput">Na pewno chcesz usunąć ten post? <a href="'+href+'" id="post_shoutbox_delete" class="InfoBoxButton">Tak</a> <a href="javascript:void(0)" id="post_shoutbox_cancel" class="InfoBoxButton">Nie</a></div>');
 			return false;
 		});
-		
-		
-		
+
+
+
 		/* Edycja */
 		$('.shoutbox_edit_post').live('click', function() {
 			var href = $(this).attr('href');
@@ -77,17 +77,17 @@
 				  $('form#ShoutBoxForm').html(html);
 				}, error:function(){
 					$('#ShoutBoxPosts').html('Wystąpił błąd! Odśwież stronę.');
-				}          
+				}
 			  });
-			
+
 			return false;
-			
+
 		});
-		  
+
 		setInterval(function() {
 			refresh2();
 		},120000);
-		
+
 		var refresh = false;
 		$('#ShoutBoxPosts').hover(function() {
 			if (!refresh) {
@@ -97,7 +97,7 @@
 		}, function() {
 			refresh = false;
 		});
-	  
+
 	});
 
 function refresh2() {
@@ -109,4 +109,55 @@ function refresh2() {
       $('#ShoutBoxPosts').html('Wystąpił błąd! Odśwież stronę.');
     }
   });
+}
+
+function addText(elname, wrap1, wrap2) {
+	if (document.selection) { // for IE
+		var str = document.selection.createRange().text;
+		document.forms['chat'].elements[elname].focus();
+		var sel = document.selection.createRange();
+		sel.text = wrap1 + str + wrap2;
+		return;
+	} else if ((typeof document.forms['chat'].elements[elname].selectionStart) != 'undefined') { // for Mozilla
+		var txtarea = document.forms['chat'].elements[elname];
+		var selLength = txtarea.textLength;
+		var selStart = txtarea.selectionStart;
+		var selEnd = txtarea.selectionEnd;
+		var oldScrollTop = txtarea.scrollTop;
+		//if (selEnd == 1 || selEnd == 2)
+		//selEnd = selLength;
+		var s1 = (txtarea.value).substring(0,selStart);
+		var s2 = (txtarea.value).substring(selStart, selEnd)
+		var s3 = (txtarea.value).substring(selEnd, selLength);
+		txtarea.value = s1 + wrap1 + s2 + wrap2 + s3;
+		txtarea.selectionStart = s1.length;
+		txtarea.selectionEnd = s1.length + s2.length + wrap1.length + wrap2.length;
+		txtarea.scrollTop = oldScrollTop;
+		txtarea.focus();
+		return;
+	} else {
+		insertText(elname, wrap1 + wrap2);
+	}
+}
+
+function insertText(elname, what) {
+	if (document.forms['chat'].elements[elname].createTextRange) {
+		document.forms['chat'].elements[elname].focus();
+		document.selection.createRange().duplicate().text = what;
+	} else if ((typeof document.forms['chat'].elements[elname].selectionStart) != 'undefined') { // for Mozilla
+		var tarea = document.forms['chat'].elements[elname];
+		var selEnd = tarea.selectionEnd;
+		var txtLen = tarea.value.length;
+		var txtbefore = tarea.value.substring(0,selEnd);
+		var txtafter =  tarea.value.substring(selEnd, txtLen);
+		var oldScrollTop = tarea.scrollTop;
+		tarea.value = txtbefore + what + txtafter;
+		tarea.selectionStart = txtbefore.length + what.length;
+		tarea.selectionEnd = txtbefore.length + what.length;
+		tarea.scrollTop = oldScrollTop;
+		tarea.focus();
+	} else {
+		document.forms['chat'].elements[elname].value += what;
+		document.forms['chat'].elements[elname].focus();
+	}
 }
