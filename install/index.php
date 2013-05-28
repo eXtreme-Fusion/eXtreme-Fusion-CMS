54018da9b00650b4ad040fdc12bf2142a24b18c9
Inscure <drimer.eco@gmail.com>
2013-05-28 17:52:32 +0200

    #137 - motywuje was ludzie do roboty ;)

--- install/index.php
+++ install/index.php
@@ -347,7 +347,7 @@ try
 			//DIR_SITE.'upload'.DS => FALSE,
 			//DIR_SITE.'upload'.DS.'archives'.DS => FALSE,
 			//DIR_SITE.'upload'.DS.'documents'.DS => FALSE,
-			//DIR_SITE.'upload'.DS.'images'.DS => FALSE,
+			DIR_SITE.'upload'.DS.'images'.DS => FALSE,
 			//DIR_SITE.'upload'.DS.'movies'.DS => FALSE,
 			DIR_SITE.'system'.DS.'opt'.DS.'plugins'.DS => FALSE,
 			DIR_SITE.'templates'.DS.'images'.DS => FALSE,
