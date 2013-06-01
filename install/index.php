df1f3c6bdfe28c905692ca33ebf2368c1488cc34
Inscure <drimer.eco@gmail.com>
2013-06-01 20:46:35 +0200

    Aktualizacja wersji

--- install/index.php
+++ install/index.php
@@ -36,7 +36,7 @@ error_reporting(-1);
 try
 {
 	// Instalowana wersja systemu - wyświetlana w nagłówku nawigacji
-	define('VERSION', '5.0.1-unstable');
+	define('VERSION', '5.0.1');
 
 	$HostURL = explode('install', $_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF']);
 
