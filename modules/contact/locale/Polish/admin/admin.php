5c686741ade94c6cbb555919b75316041caa3f4c
Tymcio <tymciom@gmail.com>
2013-07-02 22:42:13 +0200

    Contact module translation

--- modules/contact/locale/Polish/admin/admin.php
+++ modules/contact/locale/Polish/admin/admin.php
@@ -2,5 +2,27 @@
 
 return array(
 	'Contact' => 'Kontakt',
-	'Destination e-mail' => 'Adres e-mail odbiorcy'
+	'Destination e-mail' => 'Adres e-mail odbiorcy',
+	'For example: Technical department.' => 'Przykładowo: Dział techniczny.',
+	'Reciever\'s e-mail address:' => 'Adres e-mail odbiorcy:',
+	'E-mail address to which messages will be sent.' => 'Adres e-mail, na który będą wysyłane wiadomości.',
+	'Description above the form:' => 'Opis nad formularzem:',
+	'For example: To contact the Technical department please fill out the form below.' => 'Przykładowo: Aby skontaktować się z Działem technicznym, wypełnij poniższy formularz.',
+	'Default content:' => 'Domyślna treść:',
+	'In the box of message content you can place guidelines which the sender should include. For example: interests, job, experience.' => 'W polu do wpisania wiadomości można zamieścić wytyczne, które nadawca powinien uwzględnić. Przykładowo: zainteresowania, stanowisko pracy, używany system CMS.',
+	'Existing forms' => 'Istniejące formularze',
+	
+	
+	
+	
+	
+	
+	
+	//Notifications
+	'Contact form has been added.' => 'Formularz kontaktowy został dodany.',
+	'Error! Contact form has not been added.' => 'Wystąpił błąd podczas dodawania formularza.',
+	'Contact form has been edited.' => 'Formularz został zedytowany.',
+	'Error! Contact form has not been edited.' => 'Wystąpił błąd podczas edytowania formularza.',
+	'Contact form has been deleted.' => 'Formularz został usunięty.',
+	'Error! Contact form has not been deleted.' => 'Wystąpił błąd podczas usuwania formularza.',
 );
