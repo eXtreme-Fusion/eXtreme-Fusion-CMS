6eff70c8dc1ac1901e1116d71109f0bd97ab2dd7
Inscure <drimer.eco@gmail.com>
2013-07-10 00:04:43 +0200

    Usuniecie starego kodu

--- modules/cookies/pages/cookies.php
+++ modules/cookies/pages/cookies.php
@@ -1,15 +1,32 @@
 <?php
+/*********************************************************
+| eXtreme-Fusion 5
+| Content Management System
+|
+| Copyright (c) 2005-2013 eXtreme-Fusion Crew
+| http://extreme-fusion.org/
+|
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
+*********************************************************/
 
-// Pobranie z cache zapytania sprawdzającego czy moduł jest zainstalowany
+// Pobranie Zasad cookies z cache
 $policy = $_system->cache('cookies_policy', NULL, 'cookies', 2592000);
 
+// Brak danych w cache
 if ($policy === NULL)
 {
-	// Pobieranie ustawień z bazy danych oraz umieszczenie go w cache
+	// Pobranie Zasad Cookies z bazy danych
 	$policy = $_pdo->getField('SELECT `policy` FROM [cookies]');
 	$_system->cache('cookies_policy', $policy, 'cookies');
 }
 
+// Sprawdzanie, czy dane istnieją w cache/bazie
 if ($policy)
 {
 	$_tpl->assign('policy', $policy);
