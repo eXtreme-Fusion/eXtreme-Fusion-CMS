0e9354a4f14e6555c8141d428724d4c98a828a24
LynX88 <lynx.cze@gmail.com>
2013-07-12 13:37:18 +0200

    Update settings_synchro.php

--- locale/Czech/admin/settings_synchro.php
+++ locale/Czech/admin/settings_synchro.php
@@ -2,7 +2,10 @@
 
 return array(
 	// Konfiguracja systemu
-	'Synchro statement' => 'Kontrolování aktualizací (doporučeno)',
+	'Synchro statement' => 'Kontrolování dostupných aktualizací (doporučeno)',
 	'Synchro policy' => 'Výběrem této volby Vás bude systém informovat zprávou v administračním panelu, zda-li je dostupná aktualizace. Na web producenta budou odeslány následující informace o Vaší stránce: <ul> <li> IP adresa serveru </ li> <li> verze eXtreme-Fusion CMS </ li> <li> datum instalace systému </ li> </ ul> Výběr této volby není vyžadován.',
 	'Curl warning' => 'Curl rozšíření není povoleno. Prosím, pokračujte dalším krokem v instalaci nebo překonfigurujte PHP.',
+	'Synchronization' => 'Synchronizace',
+	'Download update' => 'Stáhnout aktualizaci',
+	'After putting files into FTP server, go here.' => 'Po odeslání souborů na FTP server, přejdi <a href=":url">zde</a>'
 );
