6e0183452029b404bdbab1ff91a3b6dc3f8f5dce
LynX88 <lynx.cze@gmail.com>
2013-07-13 11:13:05 +0200

    Update favourites.php

--- locale/Czech/admin/favourites.php
+++ locale/Czech/admin/favourites.php
@@ -1,7 +1,7 @@
 <?php defined('EF5_SYSTEM') || exit;
 
 return array(
-  'Favourites' => 'Ulubione',
-  'Update is available for this version of eXtreme-Fusion CMS.' => 'Dostępna jest aktualizacja dla tej wersji eXtreme-Fusion CMS.',  
-  'You do not have favourite items.' => 'Nie masz ulubionych podstron.'
+      'Favourites' => 'Oblíbené',
+      'Update is available for this version of eXtreme-Fusion CMS.' => 'Aktualizace je k dispozici pro tuto verzi eXtreme-Fusion CMS.',  
+      'You do not have favourite items.' => 'Nemáte žádné oblíbené podstránky.'
 );
