1551eaec432818f227305393160853919b861948
Rafał (Rafik89) Krupiński <rafik89@extreme-fusion.pl>
2013-07-23 01:50:43 +0200

    Tak to u nas musi już być, bo inaczej wyjątek ląduje zagnieżdzony w stronę...
    Signed-off-by: Rafał (Rafik89) Krupiński <rafik89@extreme-fusion.pl>

--- modules/forum/pages/forum.php
+++ modules/forum/pages/forum.php
@@ -1,69 +1,69 @@
 <?php
-chdir(dirname(__DIR__));
-
-define('F_CLASS', '.'.DS.'app'.DS);
-define('F_VIEW', '.'.DS.'views'.DS);
-define('F_SRC', '.'.DS.'src'.DS);
-
-define('F_ADMIN', 'acp');
-
-define('F_EXT', '.php');
-
-define('LEFT', FALSE);
-define('RIGHT', FALSE);
-
-function autoloader($class_name)
+try
 {
-	$class_name = strtolower($class_name);
+	chdir(dirname(__DIR__));
 
-	if (file_exists($path = F_CLASS.$class_name.F_EXT))
+	define('F_CLASS', '.'.DS.'app'.DS);
+	define('F_VIEW', '.'.DS.'views'.DS);
+	define('F_SRC', '.'.DS.'src'.DS);
+
+	define('F_ADMIN', 'acp');
+
+	define('F_EXT', '.php');
+
+	define('LEFT', FALSE);
+	define('RIGHT', FALSE);
+
+	function autoloader($class_name)
 	{
-		require_once $path;
-		return;
+		$class_name = strtolower($class_name);
+
+		if (file_exists($path = F_CLASS.$class_name.F_EXT))
+		{
+			require_once $path;
+			return;
+		}
+
+		if (file_exists($path = F_SRC.$class_name.F_EXT))
+		{
+			require_once $path;
+			return;
+		}
+
+		throw new systemException(__('Class :name does not exist',
+			array(':name' => $class_name)));
 	}
 
-	if (file_exists($path = F_SRC.$class_name.F_EXT))
+	spl_autoload_register('autoloader');
+
+	if ($_route->getAction())
 	{
-		require_once $path;
-		return;
+		$app = $_route->getAction();
+	}
+	else
+	{
+		$app = 'index';
 	}
 
-	throw new systemException(__('Class :name does not exist',
-		array(':name' => $class_name)));
-}
+	$path = F_CLASS.$app.F_EXT;
 
-spl_autoload_register('autoloader');
+	// Pobieranie wszystkich parametrów
+	$params = $_route->getParams();
 
-if ($_route->getAction())
-{
-	$app = $_route->getAction();
-}
-else
-{
-	$app = 'index';
-}
-
-$path = F_CLASS.$app.F_EXT;
-
-// Pobieranie wszystkich parametrów
-$params = $_route->getParams();
-
-if ($app === F_ADMIN && ! empty($params))
-{
-	$app = array_shift($params);
-
-	$path = F_CLASS.F_ADMIN.DIRECTORY_SEPARATOR.$app.F_EXT;
-}
-
-if (file_exists($path))
-{
-	// Załączanie klasy aplikacji
-	require_once $path;
-
-	$class_name = ucfirst($app).'_Controller';
-
-	try
+	if ($app === F_ADMIN && ! empty($params))
 	{
+		$app = array_shift($params);
+
+		$path = F_CLASS.F_ADMIN.DIRECTORY_SEPARATOR.$app.F_EXT;
+	}
+
+	if (file_exists($path))
+	{
+		// Załączanie klasy aplikacji
+		require_once $path;
+
+		$class_name = ucfirst($app).'_Controller';
+
 		// Pobieranie nazwy akcji oraz usuwanie jej z parametrów
 		$action = array_shift($params);
 
@@ -87,29 +87,29 @@ if (file_exists($path))
 
 		// Wyświetlanie strony
 		$_obj->render();
-	}
-	catch(optException $exception)
-	{
-		optErrorHandler($exception);
-	}
-	catch(systemException $exception)
-	{
-		systemErrorHandler($exception);
-	}
-	catch(userException $exception)
-	{
-		userErrorHandler($exception);
-	}
-	catch(PDOException $exception)
-	{
-		PDOErrorHandler($exception);
-	}
 
-	spl_autoload_unregister('autoloader');
-	spl_autoload_register('__autoload');
+		spl_autoload_unregister('autoloader');
+		spl_autoload_register('__autoload');
+	}
+	else
+	{
+		// Błąd 404 bez przekierowania.
+		$_route->trace(array('controller' => 'error', 'action' => 404));
+	}
 }
-else
+catch(optException $exception)
 {
-	// Błąd 404 bez przekierowania.
-	$_route->trace(array('controller' => 'error', 'action' => 404));
+	optErrorHandler($exception);
+}
+catch(systemException $exception)
+{
+	systemErrorHandler($exception);
+}
+catch(userException $exception)
+{
+	userErrorHandler($exception);
+}
+catch(PDOException $exception)
+{
+	PDOErrorHandler($exception);
 }
