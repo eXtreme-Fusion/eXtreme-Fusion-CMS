d4240479cf29a1144aafaa6cb6a560d46b9dac6a
Rafał (Rafik89) Krupiński <kontakt@rafik.eu>
2013-07-27 17:06:03 +0200

    Dodanie plików index.php w celu uniemożliwienia listowania katalogów przez serwer. Wejście w któryś z katalogów przez adres www powoduje przekierowanie na stronę główną.
    Signed-off-by: Rafał (Rafik89) Krupiński <kontakt@rafik.eu>

---
+++ docs/index.php
@@ -0,0 +1,20 @@
+<?php
+/*********************************************************
+| eXtreme-Fusion 5
+| Content Management System
+|
+| Copyright (c) 2005-2013 eXtreme-Fusion Crew
+| http://extreme-fusion.org/
+|
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
+| 
+**********************************************************/
+header('Location: http://'.$_SERVER['HTTP_HOST'].rtrim(dirname($_SERVER['PHP_SELF']), '/\\').'/../');
+exit;
+?>
