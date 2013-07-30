a66503e3163c74d3e6176726b57909d462a59824
Inscure <drimer.eco@gmail.com>
2013-07-30 12:34:54 +0200

    Porządki w plikach językowych

--- locale/Polish/admin/settings_synchro.php
+++ locale/Polish/admin/settings_synchro.php
@@ -5,10 +5,11 @@ return array(
 	'Synchro statement' => 'Sprawdzaj dostępność aktualizacji (zalecane)',
 	'Synchro policy' => '<p>Dzięki zaznaczeniu powyższej opcji, system będzie informował poprzez komunikat w Panelu Admina, czy dostępna jest aktualizacja. Do strony producenta wysyłane są następujące informacje dotyczące Twojej strony, serwera i systemu:</p> <ul><li>Adres IP serwera</li><li>Wersja eXtreme-Fusion CMS</li><li>Data instalacji systemu</li><li>Adres do strony</li><li>Numer lub nazwa portu do komunikacji z serwerem internetowym</li><li>Nazwa serwera</li></ul> <p>Zaznaczenie opcji nie jest obowiązkowe.</p>',
 	'Installation suggest' => 'Należy przejść do kolejnego etapu instalacji lub włączyć w konfiguracji PHP co najmniej jeden z wymienionych elementów.',
+	// Rozszerzone wersje
+	'After putting files into FTP server, go here.' => 'Po zamieszczeniu plików na serwerze FTP, przejdź <a href=":url">tutaj</a>',
 	// Krótkie wersje
 	'Synchronization' => 'Synchronizacja',
 	'None of the required components is available' => 'Nie znaleziono komponentu PHP umożliwiającego synchronizację.',
 	'Synchronization is not active.' => 'Synchronizacja nie jest aktywna.',
 	'Download update' => 'Pobierz aktualizację',
-	'After putting files into FTP server, go here.' => 'Po zamieszczeniu plików na serwerze FTP, przejdź <a href=":url">tutaj</a>',
 );
