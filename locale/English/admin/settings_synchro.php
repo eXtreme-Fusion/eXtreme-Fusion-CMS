9b63041ccaac67a1310ddb69422120c7fe391a06
Inscure <drimer.eco@gmail.com>
2013-07-30 23:15:31 +0200

    Poprawka html

--- locale/English/admin/settings_synchro.php
+++ locale/English/admin/settings_synchro.php
@@ -3,7 +3,7 @@
 return array(
 	// Konfiguracja systemu
 	'Synchro statement' => 'Check for updates (recommended)',
-	'Synchro policy' => '<p>By selecting option above, the system will inform you by message in Admin Panel about update availability. To the developer website, the following informations about your site will be sent:</p> <ul><li>IP address of the server</li><li>eXtreme-Fusion CMS version</li><li>System installation date</li><li>Website address</li>Number or name of port used to communiate with the server</li><li>Server name</li></ul> Selecting this option is not required.',
+	'Synchro policy' => '<p>By selecting option above, the system will inform you by message in Admin Panel about update availability. To the developer website, the following informations about your site will be sent:</p> <ul><li>IP address of the server</li><li>eXtreme-Fusion CMS version</li><li>System installation date</li><li>Website address</li><li>Number or name of port used to communiate with the server</li><li>Server name</li></ul> Selecting this option is not required.',
 	'Installation suggest' => 'Please go to the next step in the installation or turn on at least one of these elements in PHP configuration.',
 	'After putting files into FTP server, go here.' => 'After uploading files to FTP server go <a href=":url">there</a>.',
 );
