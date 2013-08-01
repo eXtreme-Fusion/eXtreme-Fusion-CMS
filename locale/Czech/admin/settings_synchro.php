090f45cfee1a5617507db2fa98c2e2b8246ca807
rafal <rafal@Rafal-Linux>
2013-08-01 17:17:06 +0200

    Poprawki w lokalach, brak przecinków... Oraz mój błąd w module....

--- locale/Czech/admin/settings_synchro.php
+++ locale/Czech/admin/settings_synchro.php
@@ -6,7 +6,7 @@ return array(
 'Synchro policy' => 'Výběrem této volby Vás bude systém informovat zprávou v administračním panelu, zda-li je dostupná aktualizace. Na web producenta budou odeslány následující informace o Vaší stránce: <ul> <li> IP adresa serveru </ li> <li> verze eXtreme-Fusion CMS </ li> <li> datum instalace systému </ li> </ ul> Výběr této volby není vyžadován.',
 'Installation suggest' => 'Prosím, přejděte k dalšímu kroku instalace nebo konfigurute PHP tak, aby obsahovalo alespoň jeden z požadovaných prvků.',
 // Rozszerzone wersje
-'After putting files into FTP server, go here.' => 'Po odeslání souborů na FTP server, přejdi <a href=":url">zde</a>'
+'After putting files into FTP server, go here.' => 'Po odeslání souborů na FTP server, přejdi <a href=":url">zde</a>',
 // Krótkie wersje
 'Synchronization' => 'Synchronizace',
 'None of the required components is available' => 'Nebyla nalezena komponenta PHP umožňující synchronizaci.',
