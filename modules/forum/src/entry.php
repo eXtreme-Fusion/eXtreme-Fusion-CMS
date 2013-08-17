c6ec681668e2b946293548210159d48d1d289fa0
Rafal (Rafik89) Krupiński <rafik89@extreme-fusion.pl>
2013-08-17 19:21:54 +0200

    Dla czytelniejszego czytania forum
    Rotacja 2 kolorów tła.
    
    Nowa metoda pozwalająca wstawić elementy do tablicy.

--- modules/forum/src/entry.php
+++ modules/forum/src/entry.php
@@ -3,7 +3,7 @@
 class Entry_Model extends Abstract_Model {
 
 	public function fetchByID($id)
-	{
+	{	
 		$entries = $this->_pdo->getData('
 			SELECT
 				e.*,
@@ -17,10 +17,24 @@ class Entry_Model extends Abstract_Model {
 			ORDER BY e.id ASC
 		', array(':id', $id, PDO::PARAM_INT));
 
-		if ( ! $this->_pdo->getRowsCount($entries))
+		if ($this->_pdo->getRowsCount($entries))
+		{	
+			$data = array(); $i = 0;
+			foreach ($entries as $index => $var)
+			{
+				$data[$index] = $var;
+				
+				HELP::array_push_associative($data[$index], array('row_color' => $i % 2 == 0 ? 'cals1' : 'cals2'));
+				
+				$i++;
+			}
+			
+			return $data;
+		}
+		else
+		{
 			return FALSE;
-
-		return $entries;
+		}
 	}
 
 	public function findByID($id)
