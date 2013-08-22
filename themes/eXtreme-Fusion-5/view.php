9194f282854f538f244160bcac0ba86176b6184c
Inscure <drimer.eco@gmail.com>
2013-08-22 22:37:59 +0200

    Komentarze do szablonu

--- themes/eXtreme-Fusion-5/view.php
+++ themes/eXtreme-Fusion-5/view.php
@@ -1,21 +1,48 @@
 <?php
 
+/**
+ * W tym pliku nie zamieszczamy kodu HTML, jedynie PHP.
+ * Poprzez $this->assign('nazwa', 'wartość'), można przekazać
+ * dane do pliku *.tpl.
+ */
 class Theme extends Themes implements Theme_Intf
 {
-	// Nagłówek strony, ustawienia paneli, stopka
+	/**
+	 * Funkcja odpowiedzialana za sposób prezentowania twojej strony.
+	 * To tutaj określasz jak są ustawione kolumny z panelami bocznymi:
+	 * czy po lewej i prawej stronie, czy obie kolumny po tej samej.
+	 *
+	 * W tym miejscu załączane jest również menu poziome,
+	 * nagłówek strony oraz stopka.
+	 *
+	 * Poszczególne elementy szablonu mogą być wyłączone z poziomu modułu, dzięki parametrom.
+	 * Z tego powodu zalecamy korzystać z warunków if ($parametr).
+	 *
+	 * templates/page.tpl
+	 */
 	public function page($logo = TRUE, $menu = TRUE, $left = TRUE, $right = TRUE, $footer = TRUE)
 	{
+		// Przekazywanie logo do szablonu
 		if ($logo) $this->assign('logo', trim($this->showBanners()));
+
+		// Przekazywanie menu do szablonu
 		if ($menu) $this->assign('menu', $this->showSubLinks('', 'menu'));
 
-		// Panele - o ile istnieją w danym położeniu
+		/**
+		 * Przekazywanie paneli do szablonu
+		 * Kolejność ustawiasz w panelu admina, Zarządzanie stroną -> Panele.
+		 */
+		 
+		// Przekazywanie kolumny z panelami z lewej strony 
 		if ($left)  $this->assign('LEFT', LEFT);
+		
+		// Przekazywanie kolumny z panelami z prawej strony 
 		if ($right) $this->assign('RIGHT', RIGHT);
 
-		// Część środkowa strony - panele górne, treść, panele dolne
+		// Część środkowa strony - panele górne, treść podstrony, panele dolne
 		$this->assign('CONTENT', TOP_CENTER.CONTENT.BOTTOM_CENTER);
 
-		// Stopka - pobieranie treści przez obiekt Sett
+		// Stopka - pobieranie treści z ustawień i przekazywanie do szablonu
 		if ($footer) $this->assign('footer', $this->obj('sett')->get('footer'));
 
 		// Wymagane informacje o autorach systemu i licencji
@@ -24,43 +51,65 @@ class Theme extends Themes implements Theme_Intf
 		// Link do Panelu Admina - widoczny tylko dla osób uprawnionych
 		$this->assign('admin_panel_link', $this->showAdminPanelLink());
 
-		// Licznik unikalnych odwiedzin - o ile jest właczony w Panelu Admina
+		/**
+		 * Licznik unikalnych odwiedzin - o ile jest właczony. 
+		 * Aby włączyć/wyłączyć przejdź do Panel admina -> Ustawienia -> Różne
+		 */
+		 
+		// Sprawdzanie, czy licznik unikalnych odwiedzin jest włączony.
+		// Jak w przypadku stopki, korzystamy z obiektu ustawień.
 		if ($this->obj('sett')->get('visits_counter_enabled'))
 		{
+			// Przekazywanie ilości unikalnych wizyt do szablonu
 			$this->assign('visits_count', $this->getVisitsCount());
 		}
 
-		// Wyświetlanie szablonu
+		// Wyświetlanie szablonu: templates/page.tpl
 		$this->render('page');
+		
 	}
 
-	// Pojedynczy panel boczny
+	/** 
+	 * Pojedynczy panel boczny
+	 *
+	 * templates/side_panel.tpl
+	 */
 	public function sidePanel($title = NULL)
 	{
+		// Przekazywanie nazwy panelu do szablonu
 		$this->assignGroup(array(
 			'title' => $title,
 			'open' => (bool) $title
 		));
 
-		// Wyświetlanie szablonu
+		// Wyświetlanie szablonu: templates/side_panel.tpl
 		$this->render('side_panel');
 	}
 
-	// Pojedynczy panel środkowy
+	/**
+	 * Pojedynczy panel środkowy
+	 *
+	 * templates/middle_panel.tpl
+	 */
 	public function middlePanel($title = NULL)
 	{
+		// Przekazywanie nazwy panelu do szablonu
 		$this->assignGroup(array(
 			'title' => $title,
 			'open' => (bool) $title
 		));
 
-		// Wyświetlanie szablonu
+		// Wyświetlanie szablonu: templates/middle_panel.tpl
 		$this->render('middle_panel');
 	}
 
 	// Dodatki do newsów
 	public function news()
 	{
-		// Addons to assign as variables for /pages/news.php -> news.tpl.
+		/**
+		 * (opcjonalne)
+		 * Addons to assign as variables for news.tpl 
+		 * from templates directory in the system destination path.
+		 */
 	}
 }
